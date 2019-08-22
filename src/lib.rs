38804a6cc99ccd6707aea27e2d39afa189b727dd
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-08-22 14:25:05 +0200

    First test showing db write

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,34 @@
 #[cfg(test)]
 mod tests {
+    use merk::{Merk, Result, TreeBatchEntry, TreeOp};
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn loading() -> Result<()> {
+        let mut db = Merk::open("./merk.db")?;
+
+        let mut batch: Vec<TreeBatchEntry> = vec![
+            (b"key", TreeOp::Put(b"value")),
+            (b"key2", TreeOp::Put(b"value2")),
+            (b"key3", TreeOp::Put(b"value3")),
+        ];
+        db.apply(&mut batch)?;
+
+        if let Some(tree) = &db.tree {
+            let hash = tree.node().hash();
+            assert_eq!("b18fa953ef750f27c04b1119d96b9932a7c8806d", hex::encode(&hash));
+        } else {
+            assert!(false, "foo");
+        }
+
+
+//        let mut batch: Vec<TreeBatchEntry> = vec![
+//            (b"key2", TreeOp::Delete),
+//        ];
+//        db.apply(&mut batch)?;
+
+//        println!("height: {}", &db.height());
+
+        // Do cleanup
+        db.destroy()
     }
 }
